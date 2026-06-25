e3b0165d718fd1c8a221dac18d0082cec066a14e
じば <ZIBA3496@gmail.com>
2026-06-25 21:03:38 +0900

    feat: 太字の解除と新規挿入を実装

--- src/extension.ts
+++ src/extension.ts
@@ -6,19 +6,54 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const insertBoldCommand = vscode.commands.registerCommand(
-    'markdown-palette.insertBold',
+    'markdown-palette.toggleBold',
     () => {
       const editor = vscode.window.activeTextEditor;
 
       if (editor) {
+        let nextText = '';
         const selection = editor.selection;
         const text = editor.document.getText(selection);
 
-        const boldText = `**${text}**`;
+        const isSelectionEmpty = text === '';
 
-        editor.edit((editBuilder) => {
-          editBuilder.replace(selection, boldText);
-        });
+        if (!isSelectionEmpty) {
+          const isBold = text.startsWith('**') && text.endsWith('**');
+
+          if (isBold) {
+            nextText = text.slice(2, -2);
+          } else {
+            nextText = `**${text}**`;
+          }
+        } else {
+          nextText = `**太字**`;
+        }
+
+        editor
+          .edit((editBuilder) => {
+            editBuilder.replace(selection, nextText);
+          })
+          .then(() => {
+            const startPos = selection.start;
+
+            let newStart: vscode.Position;
+            let newEnd: vscode.Position;
+
+            if (isSelectionEmpty) {
+              // 新しく「**太字**」を挿入したとき
+              // 開始位置を ** の分だけ右にずらす（+2）
+              newStart = startPos.translate(0, 2);
+              // 終了位置を 中の文字（太字）の長さ分だけ右にずらす（+2 + 2 = 4）
+              newEnd = newStart.translate(0, 2);
+            } else {
+              // 元々文字を選択していたとき（これまで通りの全体選択）
+              newStart = startPos;
+              newEnd = startPos.translate(0, nextText.length);
+            }
+
+            // 新しい選択範囲を適用
+            editor.selection = new vscode.Selection(newStart, newEnd);
+          });
       }
     },
   );
