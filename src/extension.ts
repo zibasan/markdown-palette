e5152fc38f57726f1f381c3e5cb252b180f9d41e
じば <ZIBA3496@gmail.com>
2026-06-25 21:22:53 +0900

    feat: 斜体の実装

--- src/extension.ts
+++ src/extension.ts
@@ -58,7 +58,60 @@ export function activate(context: vscode.ExtensionContext) {
     },
   );
 
-  context.subscriptions.push(insertBoldCommand);
+  const insertItalicCommand = vscode.commands.registerCommand(
+    'markdown-palette.toggleItalic',
+    () => {
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        let nextText = '';
+        const selection = editor.selection;
+        const text = editor.document.getText(selection);
+
+        const isSelectionEmpty = text === '';
+
+        if (!isSelectionEmpty) {
+          const isBold = text.startsWith('*') && text.endsWith('*');
+
+          if (isBold) {
+            nextText = text.slice(1, -1);
+          } else {
+            nextText = `*${text}*`;
+          }
+        } else {
+          nextText = `*斜体*`;
+        }
+
+        editor
+          .edit((editBuilder) => {
+            editBuilder.replace(selection, nextText);
+          })
+          .then(() => {
+            const startPos = selection.start;
+
+            let newStart: vscode.Position;
+            let newEnd: vscode.Position;
+
+            if (isSelectionEmpty) {
+              // 新しく「*斜体*」を挿入したとき
+              // 開始位置を * の分だけ右にずらす（+1）
+              newStart = startPos.translate(0, 1);
+              // 終了位置を 中の文字（斜体）の長さ分だけ右にずらす（+1 + 2 = 3）
+              newEnd = newStart.translate(0, 2);
+            } else {
+              // 元々文字を選択していたとき（これまで通りの全体選択）
+              newStart = startPos;
+              newEnd = startPos.translate(0, nextText.length);
+            }
+
+            // 新しい選択範囲を適用
+            editor.selection = new vscode.Selection(newStart, newEnd);
+          });
+      }
+    },
+  );
+
+  context.subscriptions.push(insertBoldCommand, insertItalicCommand);
 }
 
 // This method is called when your extension is deactivated
