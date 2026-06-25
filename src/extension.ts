d443451c80ded71e66346e1ca17ac3cb580df195
じば <ZIBA3496@gmail.com>
2026-06-25 19:39:12 +0900

    feat: 太字の挿入ボタンを右上の領域に追加

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,25 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const insertBoldCommand = vscode.commands.registerCommand(
+    'markdown-palette.insertBold',
+    () => {
+      const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-palette" is now active!');
+      if (editor) {
+        const selection = editor.selection;
+        const text = editor.document.getText(selection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('markdown-palette.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from markdown-palette!');
-	});
+        const boldText = `**${text}**`;
 
-	context.subscriptions.push(disposable);
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, boldText);
+        });
+      }
+    },
+  );
+
+  context.subscriptions.push(insertBoldCommand);
 }
 
 // This method is called when your extension is deactivated
